b5eb012db68602ad49c42a24cefada78639c3c5e
Niayesh Ilkhani <n.ilkhani@mail.utoronto.ca>
2017-05-02 16:00:15 -0400

    Add module license

--- generator.py
+++ generator.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2017 Niayesh Ilkhani
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Submeter Bill Generator
+
+This module is designed to take an Excel sheet of tenant data, as well
+as a utility bill template, and produce bills for every tenant for the
+selected billing period.
+"""
+
 from os import environ, makedirs, listdir, rmdir
 from os.path import exists, abspath
 from subprocess import Popen
